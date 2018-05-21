f625fa112745a92bc61e2796d6adff98a9f80105
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-21 01:54:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,6 +62,18 @@ function total() {
 }
 
 function removeFromCart(item) {
+  
+ for(let i = 0; i < cart.length; i++){
+    if (cart[i].hasOwnProperty(item) === true){
+        cart.splice(i, 1)
+        return cart
+    }
+    if()
+   }
+
+  console.log (`That item is not in your cart.`)
+  return cart 
+  
   for(let i = 0; i < cart.length; i++)
   {
     var val1 = Object.keys(cart[i])[0]
