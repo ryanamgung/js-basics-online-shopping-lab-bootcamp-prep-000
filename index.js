2e3c681e489f6c20db42db5b9b89ead5461a1a31
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-21 02:10:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,7 +79,8 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }else
   {
+    var totalCost = total()
     cart.length = 0
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${totalCost()}, which will be charged to the card ${cardNumber}.`
   }
 }
