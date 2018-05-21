50cd080216ac4447072770ac90f95b39147e70d1
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-21 02:08:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(cardNumber === null)
+  if(!Number.isInteger(cardNumber))
   {
     return `Sorry, we don't have a credit card on file for you.`
   }else
