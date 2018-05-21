4f9ba5980caef7f93d44a9aa82123b043fdb54fa
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-21 02:28:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function removeFromCart(item) {
     var val1 = Object.keys(cart[i])[0]
     var valItem = cart[i][val1]
     if (valItem === item){
-        cart.splice(valItem, 1)
+        cart.splice(cart[i], 1)
         return cart
     }
   }
